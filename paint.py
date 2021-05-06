b75fd1915b61afdf655eb24f5b618ddd35666d22
SamirH21 <A01750124@itesm.mx>
2021-05-06 11:35:13 -0500

    Formato y comentarios agregados

--- paint.py
+++ paint.py
@@ -1,5 +1,7 @@
 from turtle import *
 from freegames import vector
+
+
 #Se insertan las librerías necesarias
 def line(start, end):
     "Draw line from start to end."
@@ -8,19 +10,21 @@ def line(start, end):
     down() #Bajar la pluma y dibujar
     goto(end.x, end.y) #Posición x,y
 
+
 def square(start, end):
     "Draw square from start to end."
     up()
     goto(start.x, start.y)
     down()
     begin_fill()
-
     for count in range(4): #Trazar 4 lineas
         forward(end.x - start.x)
         left(90) #Rotar 90 grados
 
     end_fill()
 #Se define la función para hacer el cuadrado
+
+
 def circle(start, end):
     "Draw circle from start to end."
     up()
@@ -48,14 +52,13 @@ def rectangle(start, end):
     goto(start.x, start.y)
     down()
     begin_fill()
-
     for count in range(2):
         forward(end.x - start.x)
         left(90)
         forward(end.y - start.y)
         left(90)
-
     end_fill()
+
 
 def triangle(start, end):
     "Draw triangle from start to end."
@@ -63,15 +66,15 @@ def triangle(start, end):
     goto(start.x, start.y)
     down()
     begin_fill()
-
     for count in range(3):
         forward(end.x - start.x)
         left(120)
     end_fill()
+
+
 def tap(x, y):
     "Store starting point or draw shape."
     start = state['start']
-
     if start is None:
         state['start'] = vector(x, y)
     else:
@@ -80,24 +83,32 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
+
 def store(key, value):
     "Store value in state at key."
     state[key] = value
+
 
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
 listen()
-onkey(undo, 'u')
-onkey(lambda: color('black'), 'K')
+
+
+onkey(undo, 'u') # Se define la tecla para des hacer cambios
+
+
+onkey(lambda: color('black'), 'K') #Se definen las teclas para cmabiar de color y los colores
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: color('violet'), 'V') #este es el color agregado
-onkey(lambda: store('shape', line), 'l')
+
+
+onkey(lambda: store('shape', line), 'l') #Se definen las teclas para cambiar de figuras y las figuras
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
-done()
+done() #Fin del programa
